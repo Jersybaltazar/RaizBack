86fac99d03a6f9aa3604d287e2861c0e162f07e7
Jersy Baltazar <jersy.baltazar.c@gmail.com>
2024-06-12 18:04:35 -0500

    rto

---
+++ services/order.service.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Response } from "express";
+import OrderModel from "../models/order.model";
+import { catchAsyncError } from "../middleware/catchAsyncError";
+
+//create new order
+export const newOrder = catchAsyncError(
+  async (data: any, res: Response) => {
+    const order = await OrderModel.create(data);
+
+    res.status(201).json({
+      success: true,
+      order,
+    });
+  }
+);
+
+//get all Orders
+export const getAllOrdesService = async( res:Response) => {
+    const orders = await OrderModel.find().sort({createdAt: -1});
+    res.status(201).json({
+        success:true,
+        orders,
+    });
+};
