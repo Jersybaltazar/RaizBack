507f68886f08f01682132f7204fff4b03211ea1d
Jersy Baltazar <jersy.baltazar.c@gmail.com>
2024-06-19 06:00:54 -0500

    finala

--- app.ts
+++ app.ts
@@ -19,14 +19,14 @@ app.use(cookieParser());
 
 // Configuración de CORS
 const corsOptions = {
-  origin: ["https://raiz-front.vercel.app"],
+  origin: ["*"],
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 // Configuración de cabeceras CORS adicionales
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://raiz-front.vercel.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
